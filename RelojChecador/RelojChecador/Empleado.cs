585d9f7e99c1d89ae68666db756931286980e733
OliverSnE Burgara <sinue_oliver@hotmail.com>
2018-09-07 00:21:16 -0700

    Class Personas y Class Empleados listos

---
+++ RelojChecador/RelojChecador/Empleado.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelojChecador
+{
+     class Empleado : Persona
+    {
+        public enum Turno { M, V, N };
+        public Turno turno;
+        public enum Puesto { intendecia, maestro, asistente, rector, velador};
+        public Puesto puesto;
+
+        public Empleado(string nom, string emp, string cel, Turno t, Puesto p = Puesto.maestro)
+        {
+            this.nombre = nom;
+            this.empleado = emp;
+            this.celular = emp;
+            this.turno = t;
+            this.puesto = p;
+        }
+        public bool nuevo(Turno t, Puesto p = Puesto.maestro)
+        {
+            bool res = false;
+            try
+            {
+                Console.WriteLine("Ingrese el tuno: ");
+                string turno = t.ToString();
+                turno = Console.ReadLine();
+                Console.WriteLine("Ingrese el puesto: ");
+                string puesto = p.ToString();
+                puesto = Console.ReadLine();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("No fue posible realizar el registro: "+ex.Message);
+            }
+            return res;
+        }
+        public bool modificar (Turno t, Puesto p)
+        {
+            bool res = false;
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("No fue posible modificar el registro: "+ex.Message);
+            }
+
+            return res;
+        }
+        //public Empleado consulta (string criterio)
+        //{
+        //    Empleado empleado ;
+        //    Console.WriteLine("Ingrese el número de empleado: ");
+        //    criterio = Console.ReadLine();
+            
+
+        //    return empleado;
+        //}
+    }
+}
---
+++ RelojChecador/RelojChecador/Persona.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelojChecador
+{
+    class Persona
+    {
+        protected string nombre;
+        protected string empleado;
+        protected string celular;
+        
+        public bool nuevo(string nombre, string empleado, string celular)
+        {
+            bool res = false;
+            try
+            {
+                Console.WriteLine("Ingrese Número de empleado: ");
+                empleado = Console.ReadLine();
+                Console.WriteLine("Ingrese el nombre: ");
+                nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese Número de celular: ");
+                celular = Console.ReadLine();
+                res = true;
+            }catch (Exception ex)
+            {
+                Console.WriteLine("No se pudo realizar el registro: " + ex.Message);
+            }
+            return res;
+        }
+        public bool modificar (string nombre, string empleado, string celular)
+        {
+            bool res = false;
+            try
+            {
+                Console.WriteLine("Ingrese Número de empleado: ");
+                empleado = Console.ReadLine();
+                Console.WriteLine("Ingrese el nombre: ");
+                nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese Número de celular: ");
+                celular = Console.ReadLine();
+                res = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("No se pudo modificar el registro: " + ex.Message);
+            }
+
+            return res;
+        }
+        public bool eliminar (string empleado)
+        {
+            bool res = false;
+            try
+            {
+                Console.WriteLine("Ingrese el número del empleado a eliminar: ");
+                empleado = Console.ReadLine();
+
+            }catch(Exception ex)
+            {
+                Console.WriteLine("No fue posible eliminar el registro: "+ex.Message);
+            }
+            return res;
+        }
+        //public Persona consultar (string criterio)
+        //{
+        //    Persona per;
+        //    try
+        //    {
+        //        Console.WriteLine("Ingrese el numero de expediente para realiar la consulta: ");
+        //        criterio = Console.ReadLine();
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("No fue posible realizar la consulta: "+ex);
+        //    }
+
+        //    return per;
+        //}
+    }
+}
